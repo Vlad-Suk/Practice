8791e7c849ff425f000ec1ee197eb5a91648218a
Vlad-Suk <sukvladik00@gmail.com>
2022-05-30 15:11:56 +0300

    Practice with Middleware

---
+++ Middleware.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice
+{
+    public class QueryStringMiddleware
+    {
+        private RequestDelegate next;
+
+        public QueryStringMiddleware(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == "GET" && context.Request.Query["custom"] == "true")
+            {
+                if (!context.Response.HasStarted)
+                {
+                    context.Response.ContentType = "text/plain";
+                }
+                await context.Response.WriteAsync("Middleware with QueryStringMiddleware \n");
+            }
+            await next(context);
+        }
+    }
+}
