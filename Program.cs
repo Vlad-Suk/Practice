48ecf68aa9e10e3803f2830b66c7d42e7e807ac2
Vlad-Suk <sukvladik00@gmail.com>
2022-05-30 23:52:51 +0300

    Practice with pipeline

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
+app.Use(async (context, next) =>
+{
+    await next();
+    await context.Response.WriteAsync($"\n StatusCode - {context.Response.StatusCode}");
+});
+
 app.Use(async (context, next) => 
 { 
     if (context.Request.Method == HttpMethods.Get
