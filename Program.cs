db9a9260d6697c87dbfe3dc562c29da8fdc332a2
Vlad-Suk <sukvladik00@gmail.com>
2022-06-06 19:34:26 +0300

    UseEndpoints

--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@ var app = builder.Build();
 app.UseMiddleware<Population>();
 app.UseMiddleware<Capital>();
 
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("routing", async context =>
+    {
+        await context.Response.WriteAsync("Request was routed");
+    });
+});
+
 app.Run(async (context) =>
 {
     await context.Response.WriteAsync("Terminal middleware reached");
